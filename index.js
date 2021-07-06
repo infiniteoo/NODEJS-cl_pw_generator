084d799cdb528bd06957b994b300b9f2a559881e
Troy Dorman <troydorman@gmail.com>
2021-07-06 07:59:10 -0600

    created two more options to give the user ability to generate passwords without symbols or numbers by using -ns or -nn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ program.version("1.0.0").description("Simple Password Generator");
 program
   .option("-l, --length <number>", "length of password", "8")
   .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
   .parse();
 
 console.log(program.opts());
